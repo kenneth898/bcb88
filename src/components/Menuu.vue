c7c51d2c0e88e1ff8ee456c3befd4e78d56fe5bf
kenneth898 <kennethtan0601@gmail.com>
2024-09-10 17:37:52 +0800

    api sitemap robots content

--- src/components/Menuu.vue
+++ src/components/Menuu.vue
@@ -156,21 +156,62 @@
 	</p>
 
 	<p style="color: #fff;">
-		<a style="font-weight: 900; color:#FFD700;" href="https://www.atascasinobet.com/">{{
+		<a a :href="link" style="font-weight: 900; color:#FFD700;">{{
 			$t('Footer.1') }}</a>
 		| {{ $t('Footer.2') }}
 	</p>
 
 	<p style="padding-bottom: 80px; color: #fff;">
-		<a style="font-weight: 900;color:#FFD700; " href="https://www.ataskasino.com/">{{ $t('Footer.3')
+		<a a :href="link2" style="font-weight: 900;color:#FFD700; ">{{ $t('Footer.3')
 			}}</a>
 		| {{ $t('Footer.4') }}
 	</p>
 
 </template>
 <script>
+import axios from 'axios';
 export default {
-	name: 'Content'
+	name: 'Content',
+	data() {
+		return {
+			link: "",
+			link2: ""
+		}
+	},
+	methods: {
+		async calllink() {
+			try {
+				const response = await axios.get('https://seo.mobileapplab.online/api/atas?fields[0]=ataskasino_com', {
+					headers: {
+						"Authorization": "Bearer " + "1c4db3188ab2e9a077928920d9cc8d3322d15f9751bc2054a5cb70008df79cf3e3a4dd005a75a1f2db40eb953292ee10ef699693e96e9d77a98439f438ee6a6e6805a8a955e992f082b9e6118a4345e1ed18438ff9789edf9ed1dd58af45ee6669a7519a1291746959ff45bc2054b7f408b5da5ea8cd04d588a2704b7e218021",
+					}
+				});
+				this.link = response.data.data.attributes.ataskasino_com;
+
+				console.log(this.link);
+			} catch (error) {
+				console.error(error);
+			}
+		},
+		async calllink2() {
+			try {
+				const response = await axios.get('https://seo.mobileapplab.online/api/atas?fields[0]=atascasinobet_com', {
+					headers: {
+						"Authorization": "Bearer " + "1c4db3188ab2e9a077928920d9cc8d3322d15f9751bc2054a5cb70008df79cf3e3a4dd005a75a1f2db40eb953292ee10ef699693e96e9d77a98439f438ee6a6e6805a8a955e992f082b9e6118a4345e1ed18438ff9789edf9ed1dd58af45ee6669a7519a1291746959ff45bc2054b7f408b5da5ea8cd04d588a2704b7e218021",
+					}
+				});
+				this.link2 = response.data.data.attributes.atascasinobet_com;
+
+				console.log(this.link2);
+			} catch (error) {
+				console.error(error);
+			}
+		},
+	},
+	mounted() {
+		this.calllink();
+		this.calllink2();
+	}
 }
 </script>
 <style>
